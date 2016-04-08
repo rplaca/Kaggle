27577a330d07a22eb45f89c0ea94018d8f6dfca2
rplaca <johan.lindberg@pulp.se>
2016-04-08 22:30:48 +0200

    Finalized the first stab at brute force searching through the features.

--- BNP Paribas Cardif Claims Management/feat_id.py
+++ BNP Paribas Cardif Claims Management/feat_id.py
@@ -24,37 +24,68 @@ def convert_to_numeric(df):
     return df
 
 def data():
+    cols = ['v8', 'v33', 'v46', 'v53', 'v54', 'v60', 'v63', 'v64', 'v76',
+            'v89', 'v95', 'v96', 'v105', 'v116', 'v118', 'v121', 'v128']
+
     print "data"
     train_df = convert_to_numeric(pd.read_csv("train.csv")).fillna(-1)
 
+    print "dropping %d columns" % len(cols)
+    train_df = train_df.drop(cols, axis = 1)
+
+    print "chipping out a validation set"
     cut = int(len(train_df) * 0.8)
     validation_df = train_df[cut:]
     train_df = train_df[:cut]
 
+    print "done. let the madness begin"
     return train_df, validation_df
 
 def model(df):
-    print "model"
     C = AdaBoostClassifier()
     model = C.fit(df.values[:,2:],
                   df.values[:,1])
     return model
 
 def predict(model, df):
-    print "predict"
     result =  model.predict_proba(df.values[:,2:])
     return result[:,1]
 
 train_df, validation_df = data()
 
-curr_score = 0
+curr_score = log_loss(validation_df.values[:,1],
+                      predict(model(train_df), validation_df))
+base_score = curr_score
+print [], base_score, "*"
+
 best_score = 1
 cols = train_df.columns[2:]
 drop = []
-while curr_score < best_score:
+while True:
+    curr_col = None
     for col in cols:
-        _train_df = train_df.drop(drop + [col,], axis = 1)
-        _validation_df = validation_df.drop(drop + [col,], axis = 1)
-        result = log_loss(validation_df.values[:,1].astype(int),
-                          predict(model(_train_df), _validation_df))
-        print col, result
+        if col not in drop:
+            _train_df = train_df.drop(drop + [col,], axis = 1)
+            _validation_df = validation_df.drop(drop + [col,], axis = 1)
+            result = log_loss(validation_df.values[:,1].astype(int),
+                              predict(model(_train_df), _validation_df))
+            print drop + [col,], result,
+            if result < curr_score:
+                curr_col = col
+                curr_score = result
+                print "*",
+            print
+
+    if curr_score < best_score:
+        best_score = curr_score
+        drop.append(curr_col)
+
+    else:
+        break
+
+print "no more improvement."
+print "base score = %s" % (base_score)
+print "best score = %s" % (best_score)
+
+print drop
+print "stopping."
